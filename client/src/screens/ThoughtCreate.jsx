4041abf32f5977577227e8e1782eefadb52ae621
Sutton <suttondemlong@gmail.com>
2020-12-04 15:54:47 -0500

    Fix typo in currentUser

--- client/src/screens/ThoughtCreate.jsx
+++ client/src/screens/ThoughtCreate.jsx
@@ -78,9 +78,13 @@ function ThoughtCreate(props) {
       </div>
       <br />
       {
-      props.currentuser ? 
-          <button className="button" id="keep-button">keep</button> :
-          <button className="button" id="keep-button"><Link className="link" id='keep-link' to="/login">keep</Link></button>
+      props.currentUser ? 
+            <button className="button" id="keep-button">keep</button>
+            :
+            <button
+              className="button"
+              id="keep-button">
+              <Link className="link" id='keep-link' to="/login">keep</Link></button>
       }
       <button className="button" id="trash-button" onClick={trashThought}>trash</button>
     </form>
